49c43b144c2c41c34059ce5c716f95e504baca1e
YOzhik-S <yozhik-s@ya.ru>
2021-08-16 22:21:40 +0300

    add home work 2

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?
-class Product {
+abstract class Product {
 	private $id;
 	private $title;
 	private $image;
 	private $price;
+	private $costs;
 
 	public function __construct($id = NULL, $title = "", $image = "", $price = 0){
 		$this->id = $id;
@@ -35,100 +36,78 @@ class Product {
 			<p>Цена: {$this->price}руб.</p>
 			</div>";
 	}
+
+	abstract function getSumm();
+
+	abstract function getProfit();
 }
 
-class CartProduct extends Product {
+class DigitalProduct extends Product {
 	private $count;
 
-	public function __construct($product, $count = 0){
-		parent::__construct($product->getId(), $product->getTitle(), $product->getImage(), $product->getPrice());
+	public function __construct($id = NULL, $title = "", $image = "", $price = 0, $count = 0){
+		parent::__construct($id, $title, $image, $price);
 		$this->count = $count;
-	}
-
-	public function getCount(){
-		return $this->count;
-	}
-
-	public function getId(){
-		return parent::getId();
-	}
-
-	public function getTitle(){
-		return parent::getTitle();
-	}
-
-	public function getImage(){
-		return parent::getImage();
-	}
-
-	public function getPrice(){
-		return parent::getPrice();
 	}
 
 	public function getSumm(){
 		return $this->count*parent::getPrice();
 	}
 
-	public function render(){
-		return "<div id='{$this->getId()}'>
-			<p>Товар: {$this->getTitle()}</p>
-			<p>Цена: {$this->getPrice()}руб.</p>
-			<img src='{$this->getImage()}'>
-			<p>Количество: {$this->count}</p>
-			<p>Стоимость: {$this->getSumm()}руб.</p>
-			</div>";
+	public function getProfit(){
+		$ratio = 0.5;
+		return $this->getSumm()*$ratio;
 	}
 }
 
-$product1 = new Product(1,"beer", "https://shanghai-paper.com/wp-content/uploads/2020/06/Beer-in-China-696x348.jpg", 100);
+class PieceProduct extends Product {
+	private $count;
 
-$product2 = new CartProduct($product1, 100);
+	public function __construct($id = NULL, $title = "", $image = "", $price = 0, $count = 0){
+		parent::__construct($id, $title, $image, $price);
+		$this->count = $count;
+	}
 
-echo $product1->render();
+	public function getSumm(){
+		return $this->count*parent::getPrice();
+	}
+
+	public function getProfit(){
+		$ratio = 1;
+		return $this->getSumm()*$ratio;
+	}
+}
+
+class BulkProduct extends Product {
+	private $count;
+
+	public function __construct($id = NULL, $title = "", $image = "", $price = 0, $count = 0){
+		parent::__construct($id, $title, $image, $price);
+		$this->count = $count;
+	}
+
+	public function getSumm(){
+		return $this->count*parent::getPrice();
+	}
+
+	public function getProfit(){
+		$ratio = 0.02*$this->count;
+		return $this->getSumm()*$ratio;
+	}
+}
+
+// $product1 = new Product(1,"Лицензия", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.18172-8/14542459_653451241480571_1109330217809083179_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=6e5ad9&_nc_ohc=BGeq-bjk15gAX8CNZcC&_nc_ht=scontent-arn2-1.xx&oh=dcbaf4f949e1af0f08c2f621bd10e0bc&oe=613F659C", 15);
+// $product2 = new Product(2,"Пыво", "https://shanghai-paper.com/wp-content/uploads/2020/06/Beer-in-China-696x348.jpg", 35);
+// $product3 = new Product(3,"Соль", "https://images.ru.prom.st/843914750_w700_h500_sol-1-pomol.jpg", 0.0139);
+
+$cartProduct1 = new DigitalProduct(1,"Лицензия", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.18172-8/14542459_653451241480571_1109330217809083179_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=6e5ad9&_nc_ohc=BGeq-bjk15gAX8CNZcC&_nc_ht=scontent-arn2-1.xx&oh=dcbaf4f949e1af0f08c2f621bd10e0bc&oe=613F659C", 15, 10);
+$cartProduct2 = new DigitalProduct(2,"Пыво", "https://shanghai-paper.com/wp-content/uploads/2020/06/Beer-in-China-696x348.jpg", 35, 24);
+$cartProduct3 = new DigitalProduct(3,"Соль", "https://images.ru.prom.st/843914750_w700_h500_sol-1-pomol.jpg", 0.0139, 2863);
 
 echo "Товар из корзины:";
-echo $product2->render();
-
-
-class A {
-    public function foo() {
-        static $x = 0; //единственный экземпляр для всех объектов данного класса
-        echo ++$x; 
-    }
-}
-$a1 = new A();
-$a2 = new A();
-$a1->foo();
-$a2->foo();
-$a1->foo();
-$a2->foo();
-
-class A {
-    public function foo() {
-        static $x = 0; 
-        echo ++$x;
-    }
-}
-class B extends A { //наследование класса (и метода) приводит к тому, что всё-таки создается новый метод
-}
-$a1 = new A();
-$b1 = new B(); 
-$a1->foo(); 
-$b1->foo(); 
-$a1->foo(); 
-$b1->foo(); 
-
-class A {
-    public function foo() {
-        static $x = 0;
-        echo ++$x;
-    }
-}
-class B extends A { 
-}
-$a1 = new A; //не нашел в чем особенность, из курса ООП забыл в чем разница A() или A
-$b1 = new B;
-$a1->foo(); 
-$b1->foo(); 
-$a1->foo(); 
-$b1->foo(); 
+echo $cartProduct1->render();
+echo "общая стоимость: ".$cartProduct1->getSumm()."руб. прибыль от товара ".$cartProduct1->getProfit()."руб.";
+echo $cartProduct2->render();
+echo "общая стоимость: ".$cartProduct2->getSumm()."руб. прибыль от товара ".$cartProduct2->getProfit()."руб.";
+echo $cartProduct3->render();
+echo "общая стоимость: ".$cartProduct3->getSumm()."руб. прибыль от товара ".$cartProduct3->getProfit()."руб.";
